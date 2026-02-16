87f1bf2a8ef102183e806f5da9838eb47629898c
github-action-benchmark <github@users.noreply.github.com>
2026-02-16 17:01:44 +0000

    add UltrafastSecp256k1 Performance (customSmallerIsBetter) benchmark result for d2f4ffa716286af41ea4a9062abb780319faf1d8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1771261013109,
+  "lastUpdate": 1771261304620,
   "repoUrl": "https://github.com/shrec/UltrafastSecp256k1",
   "entries": {
     "UltrafastSecp256k1 Performance": [
@@ -1167,6 +1167,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Point Double",
             "value": 577,
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "payysoon@gmail.com",
+            "name": "vano",
+            "username": "shrec"
+          },
+          "committer": {
+            "email": "payysoon@gmail.com",
+            "name": "vano",
+            "username": "shrec"
+          },
+          "distinct": true,
+          "id": "d2f4ffa716286af41ea4a9062abb780319faf1d8",
+          "message": "Merge dev into main: batch affine ops (CPU/CUDA/Metal/OpenCL), hash_accel, CI fixes",
+          "timestamp": "2026-02-16T21:00:27+04:00",
+          "tree_id": "c601ae06f81920a58af0208e68a934502df81cd7",
+          "url": "https://github.com/shrec/UltrafastSecp256k1/commit/d2f4ffa716286af41ea4a9062abb780319faf1d8"
+        },
+        "date": 1771261303491,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Field Mul",
+            "value": 58,
+            "unit": "ns"
+          },
+          {
+            "name": "Field Square",
+            "value": 56,
+            "unit": "ns"
+          },
+          {
+            "name": "Field Add",
+            "value": 21,
+            "unit": "ns"
+          },
+          {
+            "name": "Field Sub",
+            "value": 16,
+            "unit": "ns"
+          },
+          {
+            "name": "Point Double",
+            "value": 571,
             "unit": "ns"
           }
         ]
